c0bf267824ddbd45450444727b36b32240f0d9f0
Carlos Gutiérrez Palmeiro <carlos.gutierrez10@alu.uclm.es>
2023-04-01 17:45:11 +0200

    Base de datos hecha y rellena, menú con funcionalidad también

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Fase2-Conflicto
 
+Importante poner claves de api en el archivo config.py
+
+## Instalaciones previas
 pip install opencv-python
 pip install matplotlib
 pip install numpy
--- src/main.py
+++ src/main.py
@@ -2,22 +2,23 @@
 
 from satelitalAnalysis import Analysis, Capture
 from meteorologico import estadoTiempo
-# import sqlite3
 
 if __name__ == '__main__':
 
-    lon = "-78.6382"
-    lat = "35.7796"
-
-    estadoTiempo(lon, lat).get_info()
-
-    # conn = sqlite3.connect('../db/database.db')
-    # c = conn.cursor()
-    # c.execute('CREATE TABLE IF NOT EXISTS presas (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, ubicacion TEXT, potencia INTEGER, latitud REAL, longitud REAL, max REAL, min REAL, indice_agua REAL, indice_area TEXT)')
-    
-    # route = '../images/imagen_satelital.jpg'
-    # Capture(-74.0, 4.5, -74.1, 4.6, route).capture()
-
-    # analysis = Analysis(route)
-    # analysis.get_water_volume()
-    # analysis.show_results()
+    while True:
+        option = input("Introduce una opción (1 para medir el tiempo meteorológico, 2 para ver la foto de un embalse, 0 para salir): ")
+        if option == "1":
+            lon = input("Introduce la longitud: ")
+            lat = input("Introduce la latitud: ")
+            estadoTiempo(lon, lat).get_info()
+        elif option == "2":
+            route = '../images/imagen_satelital.jpg'
+            Capture(-74.0, 4.5, -74.1, 4.6, route).capture()
+            analysis = Analysis(route)
+            analysis.get_water_volume()
+            analysis.show_results()
+        elif option == "0":
+            print("Saliendo del sistema...")
+            break
+        else:
+            print("Opción no válida. Por favor, introduce 1, 2 o 0.")
